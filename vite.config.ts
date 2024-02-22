87f3818dce773655eb7494790f5dea9650aee0d0
Nazar Matsevych <nazar6192111@gmail.com>
2024-02-22 18:34:35 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  base: '/parcel-creator-app/',
+  base: '/create-parcel-app/',
   plugins: [
     vue(),
     AutoImport({
